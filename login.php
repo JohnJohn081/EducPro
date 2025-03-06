a9038414c2040e1b93b0598ae337bd9e32ad9947
JohnJohn081 <johnmaclebio@gmail.com>
2025-03-05 22:17:24 -0300

    add logica back-end login pelo email

--- login.php
+++ login.php
@@ -2,19 +2,21 @@
     include("conexao.php");
     session_start();
 
-    $user = $_REQUEST["nameUser"];
+    $userOrEmail = $_REQUEST["nameUser"];
     $password = $_REQUEST["senhaUser"];
 
-    $sql = "SELECT usuario, passwordUser FROM pessoas WHERE usuario = ? AND passwordUser = ?";
+    $sql = "SELECT usuario FROM pessoas WHERE (usuario = ? OR emailUser = ?) AND passwordUser = ?";
 
     $stmt = mysqli_prepare($conn, $sql); // aqui ele prepara então add a conexão do conn e o sql
 
-    mysqli_stmt_bind_param($stmt,"ss", $user, $password); // aqui coloca no lugar do "ss" o tipo de variavel e depois as variaveis
+    mysqli_stmt_bind_param($stmt, "sss", $userOrEmail, $userOrEmail, $password);
     mysqli_stmt_execute($stmt); // aqui ele executa o comando
     mysqli_stmt_store_result($stmt); // se for pra procurar login ou usuario coloca isso daqui
 
     // e usa isso daqui pra verificar se foi true ou false a procura do negocio
     if (mysqli_stmt_num_rows($stmt) > 0) {
+        mysqli_stmt_bind_result($stmt, $user); // aqui ele cata o user depois de logar
+        mysqli_stmt_fetch($stmt); // Pega o valor do usuário
         $_SESSION ["usuario"] = $user;
         $_SESSION["user_logado"] = true;
         header("Location: areaLogada.php");
