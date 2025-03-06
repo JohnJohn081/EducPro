572d380369600f5b1d6c499df817846578a9ed32
JohnJohn081 <johnmaclebio@gmail.com>
2025-03-05 22:07:08 -0300

    elemento de email no banco de dados (front e back)

--- cad.php
+++ cad.php
@@ -5,12 +5,13 @@
         $user = $_REQUEST["nameUser"];
         $sobreNomeUser = $_REQUEST["sobrenameUser"];
         $senhaUser = $_REQUEST["senhaUser"];
+        $emailUser = $_REQUEST["emailUser"];
 
-        $sql = "INSERT INTO PESSOAS (USUARIO, NAMEUSER, PASSWORDUSER) VALUES (?, ?, ?)";
+        $sql = "INSERT INTO PESSOAS (USUARIO, NAMEUSER, PASSWORDUSER, EMAILUSER) VALUES (?, ?, ?, ?)";
 
         $stmt = mysqli_prepare($conn, $sql);
 
-        mysqli_stmt_bind_param($stmt, "sss", $user, $sobreNomeUser, $senhaUser);
+        mysqli_stmt_bind_param($stmt, "ssss", $user, $sobreNomeUser, $senhaUser, $emailUser);
 
         if (mysqli_stmt_execute($stmt)) {
             $_SESSION["user_logado"] = true;
